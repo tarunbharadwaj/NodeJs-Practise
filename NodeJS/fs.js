5d7d4fec2a6b093290d5accf38f0b917aeab81aa
tarun bharadwaj <46241231+tarunbharadwaj@users.noreply.github.com>
2021-08-06 19:11:29 +0530

    Update fs.js

--- NodeJS/fs.js
+++ NodeJS/fs.js
@@ -1,17 +1,17 @@
 var filesystem = require('fs')
 
 //Create a file  file name     Text to be written
-fs.writefile('New Text.txt','Hello world is the sample text',function(err){
+fs.writeFile('New Text.txt','Hello world is the sample text',function(err){
     if(err) throw err;
     console.log("File Created")
 })
 
-fs.appendfile('New Text.txt',`${Math.floor(Math.random()*1000)} This is a new number \n`,function(err){
+fs.appendFile('New Text.txt',`${Math.floor(Math.random()*1000)} This is a new number \n`,function(err){
     if(err) throw err;
     console.log("Data appended")
 })
 
-fs.readfile('New Text.txt','utf-8',function(err,data){
+fs.readFile('New Text.txt','utf-8',function(err,data){
     if(err) throw err;
     console.log(data)
 })
